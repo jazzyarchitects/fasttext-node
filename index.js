3b694a4ce16024f410fe98ac96972f7bbf2d3410
Jibin Mathews <jibin.mathews@rentomojo.com>
2017-09-05 17:54:00 +0530

    Instance config

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const FastText = require(path.join(__dirname, 'dist', 'lib'));
 
 module.exports = function(...args) {
+  console.log('calling constructor', ...args);
   // eslint-disable-next-line new-cap
   return new FastText.default(...args);
 };
