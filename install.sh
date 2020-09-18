c908241b38e366976a85ca6ece69fc5ab421b483
raisinten <raisinten@gmail.com>
2020-09-18 18:00:34 +0530

    Refactored the install script a bit
    * used command -v <program-name> in place of <program-name -v>
    * reordered few comments

--- install.sh
+++ install.sh
@@ -7,29 +7,25 @@
 # Checking if python exists
 
 # Check if c++ is installed
-C_COMMAND=$(c++ --version)
-C_EXISTS=$?
 
-if [ "$C_EXISTS" -eq "0" ]
+if command -v c++ &> /dev/null
 then 
   echo -e "\033[0;32m\033[1mc++ compiler is installed... Cool...!!!\033[0m"
 else 
-  GCC_COMMAND=$(gcc --version)
-  GCC_EXISTS=$?
 
-  if [ "$GCC_EXISTS" -eq "0" ]
+  # Check if gcc is installed
+
+  if command -v gcc &> /dev/null
   then 
     echo -e "\033[0;32m\033[1mgcc c++ compiler is installed... Cool...!!!\033[0m"
   fi
+
   echo -e "\033[0;31mC++ compiler not installed in your system. Install c++ or gcc to use \033[1m\033[4mFastText-Node\033[0;31m  module \033[0m"
 fi
 
-GIT_COMMAND=$(git --version)
-
-GIT_EXISTS=$?
-
 # Checking if git exists. Git is required for cloning fast text library
-if [ "$GIT_EXISTS"  -eq "0" ]
+
+if command -v git &> /dev/null
 then
   echo -e "\033[0;32m\033[1mGit is installed... Cool...!!!\033[0m"
 else
@@ -38,11 +34,8 @@ else
 fi
 
 # Check if curl is installed
-CURL_COMMAND=$(curl --version)
 
-CURL_EXISTS=$?
-
-if [ "$CURL_EXISTS" -eq "0" ]
+if command -v curl &> /dev/null
 then
   echo -e "\033[0;32m\033[1mCurl is installed... Cool...!!!\033[0m"
 else
@@ -50,12 +43,14 @@ else
   exit 1
 fi
 
+# Installing FastText-Node Module
 
 echo -e "\033[0;32m\033[1mInstalling FastText-Node Module\033[0m"
 
 echo -e "\033[0;32mDownloading Fast Text Library\033[0m"
 
 # remove the existing folder else git throws an error
+
 rm -rf fastText/
 
 git clone https://github.com/facebookresearch/fastText.git
@@ -66,13 +61,15 @@ echo -e "Building the library\033[0m"
 cd fastText
 make
 
+echo -e "\033[0;32mBuilt Fast Text Library.\033[0m"
+
 echo -e "\033[0;32m\033[1mInstalled FastText-Node Library\033[0m"
 
 echo ""
 
-echo -e "\033[0;32m\033[1mBuilding node module\033[0m"
-
 cd ..
+
+echo -e "\033[0;32m\033[1mBuilding node module\033[0m"
 
 ROOT=$(pwd)
 
