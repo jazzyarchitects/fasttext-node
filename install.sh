1e7b8066800312790aa58ee174bb8c15dcf237b6
raisinten <raisinten@gmail.com>
2020-09-18 18:49:51 +0530

    Replaced raw colour codes with variables

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# colours
+
+PRE="\033["             # prefix
+NC="${PRE}0m"           # no colour
+LG="${PRE}0;32m"        # light green
+DG="${PRE}1;32m"        # dark green
+LR="${PRE}0;31m"        # light red
+BI="${PRE}1m${PRE}4m"   # bold italics
+
 # Python is needed only for word similarity prediction
 #PYTHON2_COMMAND=$(python --version)
 #PYTHON2_EXISTS=$?
@@ -10,26 +19,26 @@
 
 if command -v c++ &> /dev/null
 then 
-  echo -e "\033[0;32m\033[1mc++ compiler is installed... Cool...!!!\033[0m"
+  echo -e "${DG}C++ compiler is installed... Cool...!!!${NC}"
 else 
 
   # Check if gcc is installed
 
   if command -v gcc &> /dev/null
   then 
-    echo -e "\033[0;32m\033[1mgcc c++ compiler is installed... Cool...!!!\033[0m"
+    echo -e "${DG}Gcc compiler is installed... Cool...!!!${NC}"
   fi
 
-  echo -e "\033[0;31mC++ compiler not installed in your system. Install c++ or gcc to use \033[1m\033[4mFastText-Node\033[0;31m  module \033[0m"
+  echo -e "${LR}C++ compiler is not installed on your system. Install c++ or gcc to use the ${BI}FastText-Node${LR} module${NC}"
 fi
 
 # Checking if git exists. Git is required for cloning fast text library
 
 if command -v git &> /dev/null
 then
-  echo -e "\033[0;32m\033[1mGit is installed... Cool...!!!\033[0m"
+  echo -e "${DG}Git is installed... Cool...!!!${NC}"
 else
-  echo -e "\033[0;31mGit not installed in your system. Install git to use \033[1m\033[4mFastText-Node\033[0;31m  module \033[0m"
+  echo -e "${LR}Git not installed in your system. Install git to use ${BI}FastText-Node${LR} module${NC}"
   exit 1
 fi
 
@@ -37,17 +46,17 @@ fi
 
 if command -v curl &> /dev/null
 then
-  echo -e "\033[0;32m\033[1mCurl is installed... Cool...!!!\033[0m"
+  echo -e "${DG}Curl is installed... Cool...!!!${NC}"
 else
-  echo -e "\033[0;31mCurl not installed in your system. Install curl to use \033[1m\033[4mFastText-Node\033[0;31m  module \033[0m"
+  echo -e "${LR}Curl not installed in your system. Install curl to use ${BI}FastText-Node${LR} module${NC}"
   exit 1
 fi
 
 # Installing FastText-Node Module
 
-echo -e "\033[0;32m\033[1mInstalling FastText-Node Module\033[0m"
+echo -e "${DG}Installing FastText-Node Module${NC}"
 
-echo -e "\033[0;32mDownloading Fast Text Library\033[0m"
+echo -e "${LG}Downloading Fast Text Library${NC}"
 
 # remove the existing folder else git throws an error
 
@@ -55,24 +64,24 @@ rm -rf fastText/
 
 git clone https://github.com/facebookresearch/fastText.git
 
-echo -e "\033[0;32mDownloaded Fast Text Library."
-echo -e "Building the library\033[0m"
+echo -e "${LG}Downloaded Fast Text Library.${NC}"
+echo -e "${LG}Building the library${NC}"
 
 cd fastText
 make
 
-echo -e "\033[0;32mBuilt Fast Text Library.\033[0m"
+echo -e "${LG}Built Fast Text Library.${NC}"
 
-echo -e "\033[0;32m\033[1mInstalled FastText-Node Library\033[0m"
+echo -e "${DG}Installed FastText-Node Library${NC}"
 
 echo ""
 
 cd ..
 
-echo -e "\033[0;32m\033[1mBuilding node module\033[0m"
+echo -e "${DG}Building node module${NC}"
 
 ROOT=$(pwd)
 
 ${ROOT}/node_modules/.bin/grunt build
 
-echo -e "\033[0;32m\033[1mFinished installing module\033[0m"
+echo -e "${DG}Finished installing module${NC}"
